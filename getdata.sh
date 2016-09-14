79d5669892ad590bf561d8c516b9b6f30a3a7b34
Sergio Salomón <sergio.salomon@alumnos.unican.es>
2016-09-15 01:17:19 +0200

    Terminada la descarga y descompresion de los datos

--- getdata.sh
+++ getdata.sh
@@ -1,31 +1,41 @@
+#!/bin/bash
 
-## Obtencion de los datos desde el repositorio web dado
+####
+## Metodos de Gestion de la Informacion
+## Master de Matematicas y Computacion
+## Sergio Salomon Garcia   <sergio.salomon@alumnos.unican.es>
+##
+## Obtencion de los datos desde el repositorio web dado 
+## y descompresion de estos.
+####
 
+dataurl="http://www.minetur.gob.es/energia/balances/Publicaciones/ElectricasMensuales/"
+tempdir="tmp/"
 
-## Crea el repositorio objetivo		# TODO 
-mkdir data
+## Crea el directorio temporal para almacenar los datos
+mkdir $tempdir
 
+## Itera por los anhos y meses a descargar
 for y in '2006' '2007' '2008' '2009' '2010' '2011' '2012' '2013' '2014'
 do
 	for m in 'Enero' 'Febrero' 'Marzo' 'Abril' 'Mayo' 'Junio' 'Julio' 'Agosto' 'Septiembre' 'Octubre' 'Noviembre' 'Diciembre'
 	do 
 		## Guarda el mes en minuscula
-		# mesMin="$(tr [A-Z] [a-z] <<< "$m")"
-		read mesMin <<< $(awk '{print tolower($0)}' <<< $m)
+		mesMin="$(tr [A-Z] [a-z] <<< "$m")"
 
 		## Descarga de datos de prueba
-		curl http://www.minetur.gob.es/energia/balances/Publicaciones/ElectricasMensuales/$y/$m\_$y.zip -o data/$m-$y.zip >& /dev/null
+		curl $dataurl$y/$m\_$y.zip -o $tempdir$m-$y.zip >& /dev/null
 
 		## Descompresion de los datos
-		unzip data/$m-$y.zip -d data/ 	>& /dev/null
+		unzip $tempdir$m-$y.zip -d $tempdir 	>& /dev/null
 
 		## Borra el fichero descargado tras extraer los datos
-		rm data/$m-$y.zip 				>& /dev/null
+		rm $tempdir$m-$y.zip 					>& /dev/null
 
 		## Renombra los ficheros extraidos segun dos posibles patrones
-		mv data/$m data/$m-$y			>& /dev/null
-		mv data/$m\_$y data/$m-$y		>& /dev/null
-		mv "data/$mesMin $y" data/$m-$y	>& /dev/null
+		mv $tempdir$m $tempdir$m-$y				>& /dev/null
+		mv $tempdir$m\_$y $tempdir$m-$y			>& /dev/null
+		mv "$tempdir$mesMin $y" $tempdir$m-$y	>& /dev/null
 	done 
 done
 
