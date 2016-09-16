e0def7d843d594e69dcd6d8bf05c3dbe2636e6d0
ssg70 <ssg70@alumnos.unican.es>
2016-09-16 13:54:13 +0200

    Primera version de la iteracion a traves de CA-Provincias

--- mkdirs.sh
+++ mkdirs.sh
@@ -9,3 +9,20 @@
 ## Comunidad > Provincia.
 ####
 
+datadir="data/"
+hierachyfile="provincias.csv"
+
+## Crea la carpeta raiz del directorio de datos
+mkdir $datadir >& /dev/null
+
+## Itera a traves de las provicias y comunidades autonomas
+while read i
+do
+	comaut=$(printf "$i" | cut -d "," -f 2 | tr ' ' '-' | tr -d '.')
+	prov=$(printf "$i" | cut -d "," -f 1 | tr ' ' '-' | tr -d '.')
+
+	printf "$i\n"
+	printf "\t$prov\n"
+	printf "\t$comaut\n"
+done < $hierachyfile
+
