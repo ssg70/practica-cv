e4c3b40cb242f77db93d96b9e548847e2ffff799
ssg70 <ssg70@alumnos.unican.es>
2016-09-18 10:37:13 +0200

    Terminado el procesado de datos y la generacion de las series. No muy eficiente...

--- parser.sh
+++ parser.sh
@@ -9,6 +9,8 @@
 ## de las nuevas series temporales.
 ####
 
+LC_CTYPE=C
+
 datadir="data/"
 dataname="T_127P_"
 hierachyfile="provincias.csv"
@@ -25,13 +27,15 @@ serie7="total"
 printf "\n"
 
 ## Recorre los datos por anho y mes
-for y in '2006' '2007' '2008' '2009' '2010' '2011' '2012' '2013' '2014'
+for y in '2006' '2007' '2008' '2009' '2010' '2011' '2012' '2013' '2014' '2015'
 do
+	## Muestra el progreso de anhos
+	printf "\n$y"
+
 	for m in 'Enero,01' 'Febrero,02' 'Marzo,03' 'Abril,04' 'Mayo,05' 'Junio,06' 'Julio,07' 'Agosto,08' 'Septiembre,09' 'Octubre,10' 'Noviembre,11' 'Diciembre,12' 
 	do 
 		## Muestra el progreso del procesado de datos
 		printf "."
-		echo $m$y
 
 		mes=$(printf "$m" | cut -d "," -f 1)
 		mesnum=$(printf "$m" | cut -d "," -f 2)
@@ -44,9 +48,49 @@ do
 		lines=$(cat $datfile| tail -n +10 | head -n 52)
 
 		## Recorre las filas de datos
-		while read -r i
+		while read row
 		do
-			## TODO
+			## Conversion de caracteres para procesar ñ
+			row=$(iconv -f ISO-8859-1 <<< "$row")
+			row=$(sed 's/Ñ/NH/g' <<< "$row")
+
+			## Extrae la primera columna, la provincia
+			col0=$(printf "$row" | colrm 22)
+
+			## Extrae de cada columna el valor de esa serie y elimina puntos
+			col1=$(printf "$row" | colrm 1 22 | colrm 11 | tr -d '.')
+			col2=$(printf "$row" | colrm 1 35 | colrm 11 | tr -d '.')
+			col3=$(printf "$row" | colrm 1 48 | colrm 11 | tr -d '.')
+			col4=$(printf "$row" | colrm 1 61 | colrm 11 | tr -d '.')
+			col5=$(printf "$row" | colrm 1 74 | colrm 11 | tr -d '.')
+			col6=$(printf "$row" | colrm 1 87 | colrm 11 | tr -d '.')
+			col7=$(printf "$row" | colrm 1 100 | colrm 13 | tr -d '.')
+
+			## Elimina espacios anteriores y posteriores a los valores
+			col0=$(echo $col0)
+			col1=$(echo $col1)
+			col2=$(echo $col2)
+			col3=$(echo $col3)
+			col4=$(echo $col4)
+			col5=$(echo $col5)
+			col6=$(echo $col6)
+			col7=$(echo $col7)
+
+			## Transforma el valor cambiando espacios, puntos y mayusculas
+			prov=$(printf "$col0" | tr ' ' '-' | tr -d '.' | tr [A-Z] [a-z])
+
+			## Busca la comunidad autonoma
+			search=$(grep "$col0," $hierachyfile)
+			comaut=$(printf "$search" | cut -d "," -f 2 | tr ' ' '-' | tr -d '.' | tr [A-Z] [a-z])
+
+			## Guarda los valores de cada columna en las series temporales
+			printf "$y$mesnum $col1\n" >> "$datadir$comaut/$prov/$serie1"
+			printf "$y$mesnum $col2\n" >> "$datadir$comaut/$prov/$serie2"
+			printf "$y$mesnum $col3\n" >> "$datadir$comaut/$prov/$serie3"
+			printf "$y$mesnum $col4\n" >> "$datadir$comaut/$prov/$serie4"
+			printf "$y$mesnum $col5\n" >> "$datadir$comaut/$prov/$serie5"
+			printf "$y$mesnum $col6\n" >> "$datadir$comaut/$prov/$serie6"
+			printf "$y$mesnum $col7\n" >> "$datadir$comaut/$prov/$serie7"
 		done <<< "$lines"
 	done
 done
